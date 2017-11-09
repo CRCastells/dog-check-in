d04b640c8e4776dee6a1837d6dbc9da6058ea331
Quinn O'Neill <quinnoneill@Quinns-MacBook-Pro-2.local>
2017-11-09 15:56:26 -0700

    builds basic header component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
