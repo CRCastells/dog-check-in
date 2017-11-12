a8bf767de8f5045d31c01bd8fc551083d1ad797d
CRCastells <cameroncastells@gmail.com>
2017-11-12 14:20:28 -0700

    adding CORS exception for heroku

--- back-end/server.js
+++ back-end/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 //Only needed if not on Heroku/prod
 if (!process.env.DYNO) {
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "https://dogcheckin.herokuapp.com" || 'http://localhost:4200');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
     next();
